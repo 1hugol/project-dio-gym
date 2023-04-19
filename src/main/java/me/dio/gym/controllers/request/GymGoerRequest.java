d06c15c03999841a81c8a20f235964dca65d43c5
Hugo Amorim <1hugoamorim@gmail.com>
2023-04-19 20:21:21 +0000

    Implementation of improvements

--- src/main/java/me/dio/gym/controllers/request/GymGoerRequest.java
+++ src/main/java/me/dio/gym/controllers/request/GymGoerRequest.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
-import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -13,6 +12,7 @@ import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class GymGoerRequest {
     @NotBlank
     private String name;
@@ -22,31 +22,8 @@ public class GymGoerRequest {
 
     @Size(max = 9)
     private String cep;
-    @Size(max = 50)
-    private String address;
-    @Size(max = 50)
-    private String district;
-    @Size(max = 30)
-    private String city;
-    @Size(max = 2)
-    private String state;
+
     @Past
     private LocalDate birthDate;
 
-    public GymGoerRequest(String name, String cpf, String cep, LocalDate birthDate) {
-        this.name = name;
-        this.cpf = cpf;
-        this.cep = cep;
-        this.birthDate = birthDate;
-    }
-
-    public GymGoerRequest(String name, String cpf, String address, String district, String city, String state, LocalDate birthDate) {
-        this.name = name;
-        this.cpf = cpf;
-        this.address = address;
-        this.district = district;
-        this.city = city;
-        this.state = state;
-        this.birthDate = birthDate;
-    }
 }
